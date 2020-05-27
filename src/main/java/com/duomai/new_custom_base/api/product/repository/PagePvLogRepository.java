e23d4f9e2b7a6e8acfff536b97f710c1854c24d3
im-luxqi <im_luxqi@163.com>
2020-05-27 19:41:04 +0800

    使用jpa

---
+++ src/main/java/com/duomai/new_custom_base/api/product/repository/PagePvLogRepository.java
@@ -0,0 +1,10 @@
+package com.duomai.new_custom_base.api.product.repository;
+
+import com.duomai.new_custom_base.api.product.base.BaseRepository;
+import com.duomai.new_custom_base.api.product.entity.PagePvLog;
+
+import java.util.List;
+
+public interface PagePvLogRepository extends BaseRepository<PagePvLog, String> {
+    List<PagePvLog> findByBuyerNick(String buyerNick);
+}
--- src/main/java/com/duomai/new_custom_base/product/TestController.java
+++ src/main/java/com/duomai/new_custom_base/product/TestController.java
@@ -1,9 +1,13 @@
 package com.duomai.new_custom_base.product;
 
+import com.duomai.new_custom_base.api.product.entity.PagePvLog;
+import com.duomai.new_custom_base.api.product.repository.PagePvLogRepository;
 import com.duomai.new_custom_base.product.gen.service.IGenTableService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * @description
@@ -14,10 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @Autowired
     IGenTableService iGenTableService;
+    @Autowired
+    PagePvLogRepository pagePvLogRepository;
 
     @GetMapping("/test")
     public String test() {
-        iGenTableService.getById(1);
+//        iGenTableService.getById(1);
+        PagePvLog pagePvLog = new PagePvLog();
+//        pagePvLog.setId("1111111111111");
+        pagePvLog.setBuyerNick("121231");
+        pagePvLog.setPage("eee");
+        pagePvLog.setCreateTime(new Date());
+        pagePvLog.setUpdateTime(new Date());
+//        List<PagePvLog> all = pagePvLogRepository.findAll();
+        pagePvLogRepository.save(pagePvLog);
         return "success";
     }
 }
