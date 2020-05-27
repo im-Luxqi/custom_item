caa4644fe46677e7064a32e73d0b94d885e78b9e
im-luxqi <im_luxqi@163.com>
2020-05-27 10:44:54 +0800

    首次创建

---
+++ src/main/java/com/duomai/new_custom_base/NewCustomBaseApplication.java
@@ -0,0 +1,14 @@
+package com.duomai.new_custom_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCustomBaseApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewCustomBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duomai/new_custom_base/NewCustomBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duomai.new_custom_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCustomBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
