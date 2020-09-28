a73fcca3d9d986a76af975d72313b7ebae08c66c
wangxingqi <im_luxqi@163.com>
2020-09-28 19:36:48 +0800

    优化--处理报错

--- custom-item-project/src/main/java/com/duomai/project/product/TestController.java
+++ custom-item-project/src/main/java/com/duomai/project/product/TestController.java
@@ -2,15 +2,13 @@ package com.duomai.project.product;
 
 import com.duomai.project.api.taobao.ITaobaoAPIService;
 import com.duomai.project.configuration.SysCustomProperties;
-import com.duomai.project.product.general.dto.XyReturn;
-import com.duomai.project.product.general.enums.LuckyChanceFrom;
-import com.duomai.project.product.general.repository.SysAwardRepository;
-import com.duomai.project.product.general.repository.SysLuckyChanceRepository;
 import com.duomai.project.helper.LuckyDrawHelper;
 import com.duomai.project.helper.ProjectHelper;
+import com.duomai.project.product.general.dto.XyReturn;
+import com.duomai.project.product.general.repository.SysAwardRepository;
+import com.duomai.project.product.general.repository.SysLuckyChanceRepository;
 import com.taobao.api.ApiException;
 import com.taobao.api.DefaultTaobaoClient;
-import com.taobao.api.response.AlibabaBenefitSendResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,22 +54,6 @@ public class TestController {
 
     @GetMapping(value = "wxq")
     public XyReturn test() throws ApiException {
-//        ActBaseSetting actBaseSetting = projectHelper.actBaseSettingFind();
-//        String openId = "tb138805818";
-//        String buyernick = "t011OKZDdxsgi2BgiM54DcAqE4trS9hcL3O43H6xfPR9tU=";
-//        Date requestTime = new Date();
-//        List<SysAward> all = sysAwardRepository.findAllByOrderByLuckyValue();
-//        try {
-//            projectHelper.customInit(null);
-//        } catch (ApiException e) {
-//            e.printStackTrace();
-//        }
-        AlibabaBenefitSendResponse alibabaBenefitSendResponse = taobaoAPIService.sendTaobaoCoupon("AAHdN7xBAMPS9ht6emzVRz-2", "8a75f0a00dcb42f0a76905f4847eb96b",
-                sysCustomProperties.getCustomConfig().getAppName(), sysCustomProperties.getCustomConfig().getSessionkey());
-//        luckyDrawHelper.sendLuckyChance("s01bPY6OKAXoXEBLuS0WXELkWL65KSN4Xgj0x2NaS0H3n4=", LuckyChanceFrom.ORDER_COMMIT,200,"zhujinya");
-//        XyReturn ordersByOpenId = projectHelper.findOrdersByOpenId(System.currentTimeMillis(), openId,openId,
-//                actBaseSetting.getActStartTime().getTime(), actBaseSetting.getActEndTime().getTime(), buyernick, requestTime);
         return null;
-
     }
 }
