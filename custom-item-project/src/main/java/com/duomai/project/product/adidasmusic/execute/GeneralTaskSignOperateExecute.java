f38e09ef739786d4a3253572fb82d1b056e0645f
陆漪佳 <214575017@qq.com>
2020-09-29 19:08:37 +0800

    阿迪双十一：任务页面

--- custom-item-project/src/main/java/com/duomai/project/product/adidasmusic/execute/GeneralTaskSignOperateExecute.java
+++ custom-item-project/src/main/java/com/duomai/project/product/adidasmusic/execute/GeneralTaskSignOperateExecute.java
@@ -4,14 +4,19 @@ import com.duomai.common.base.execute.IApiExecute;
 import com.duomai.common.dto.ApiSysParameter;
 import com.duomai.common.dto.YunReturnValue;
 import com.duomai.project.product.general.entity.SysCustom;
+import com.duomai.project.product.general.entity.SysGeneralTask;
+import com.duomai.project.product.general.enums.TaskTypeEnum;
 import com.duomai.project.product.general.repository.SysCustomRepository;
 import com.duomai.project.product.general.repository.SysGeneralTaskRepository;
+import com.duomai.project.tool.CommonDateParseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @内容：任务页面 每日打卡操作
@@ -32,8 +37,17 @@ public class GeneralTaskSignOperateExecute implements IApiExecute {
         // 校验玩家是否存在
         SysCustom sysCustom = sysCustomRepository.findByBuyerNick(buyerNick);
         Assert.notNull(sysCustom, "不存在该玩家");
-        /*1. 校验是否打卡*/
-
-        return null;
+        // 校验
+        List<SysGeneralTask> signLog = sysGeneralTaskRepository.findByBuyerNickAndTaskTypeAndCreateTimeBetween(buyerNick,
+                TaskTypeEnum.SIGN, CommonDateParseUtil.getStartTimeOfDay(new Date()), CommonDateParseUtil.getEndTimeOfDay(new Date()));
+        if (signLog.size() > 0){
+            Assert.isNull(signLog, "重复操作！");
+        }
+        /*保存操作日志*/
+        SysGeneralTask signOpt = new SysGeneralTask();
+        sysGeneralTaskRepository.save(signOpt.setBuyerNick(buyerNick)
+                .setCreateTime(sysParm.getRequestStartTime())
+                .setTaskType(TaskTypeEnum.SIGN));
+        return YunReturnValue.ok("操作成功！");
     }
 }
