b452b2b828a7e09670ba9417b2dd901c345b328b
陆漪佳 <214575017@qq.com>
2020-09-30 14:50:21 +0800

    阿迪双十一：尖货大咖

--- custom-item-project/src/main/java/com/duomai/project/product/adidasmusic/execute/CusBigWheelListExecute.java
+++ custom-item-project/src/main/java/com/duomai/project/product/adidasmusic/execute/CusBigWheelListExecute.java
@@ -1,12 +1,23 @@
 package com.duomai.project.product.adidasmusic.execute;
 
+import com.alibaba.fastjson.JSONObject;
 import com.duomai.common.base.execute.IApiExecute;
 import com.duomai.common.dto.ApiSysParameter;
 import com.duomai.common.dto.YunReturnValue;
+import com.duomai.project.product.adidasmusic.domain.CusBigWheel;
+import com.duomai.project.product.adidasmusic.service.ICusBigWheelService;
+import com.duomai.project.product.general.dto.PageListDto;
+import com.duomai.project.product.general.enums.CusBigWheelStateEnum;
+import com.duomai.project.product.general.repository.SysCustomRepository;
+import org.hibernate.procedure.spi.ParameterRegistrationImplementor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @内容：尖货大咖 活动标签
@@ -15,8 +26,28 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Component
 public class CusBigWheelListExecute implements IApiExecute {
+    @Autowired
+    private ICusBigWheelService iCusBigWheelService;
+
     @Override
     public YunReturnValue ApiExecute(ApiSysParameter sysParm, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+//        //获取参数
+//        JSONObject object = JSONObject.parseObject(sysParm.getApiParameter().getAdmjson().toString());
+
+        PageListDto pageListDto = new PageListDto();
+        List<CusBigWheel> cusBigWheelList = iCusBigWheelService.listCusBigWheel();
+        for (CusBigWheel cusBigWheel : cusBigWheelList){
+            if (cusBigWheel.getStartTime().after(new Date())){  // 开始时间大于当前时间
+                cusBigWheel.setState(CusBigWheelStateEnum.NOT_STARTING);
+            }
+            if (cusBigWheel.getEndTime().before(new Date())){   // 结束时间小于当前时间
+                cusBigWheel.setState(CusBigWheelStateEnum.STOPPING);
+            }
+            if (cusBigWheel.getStartTime().before(new Date()) && cusBigWheel.getEndTime().after(new Date())){
+                cusBigWheel.setState(CusBigWheelStateEnum.PROGRESSING);
+            }
+        }
+        pageListDto.setResultList(cusBigWheelList);
+        return YunReturnValue.ok(pageListDto,"尖货大咖记录");
     }
 }
--- custom-item-project/src/main/java/com/duomai/project/product/adidasmusic/service/ICusBigWheelService.java
+++ custom-item-project/src/main/java/com/duomai/project/product/adidasmusic/service/ICusBigWheelService.java
@@ -3,11 +3,15 @@ package com.duomai.project.product.adidasmusic.service;
 import com.duomai.common.framework.mybatisplus.service.BaseService;
 import com.duomai.project.product.adidasmusic.domain.CusBigWheel;
 
+import java.util.List;
+
 /**
  * 【请填写功能名称】 服务层
  *
  * @author system
  */
 public interface ICusBigWheelService extends BaseService<CusBigWheel> {
+
+    List<CusBigWheel> listCusBigWheel();
 
 }
