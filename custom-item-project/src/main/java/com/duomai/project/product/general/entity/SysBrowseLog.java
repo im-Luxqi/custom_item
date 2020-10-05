fe3eea51ce2c15fad54b302a15d2f9581dd17710
陆漪佳 <214575017@qq.com>
2020-10-05 10:52:30 +0800

    阿迪双十一：任务页面

--- custom-item-project/src/main/java/com/duomai/project/product/general/entity/SysBrowseLog.java
+++ custom-item-project/src/main/java/com/duomai/project/product/general/entity/SysBrowseLog.java
@@ -2,11 +2,9 @@ package com.duomai.project.product.general.entity;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 /**
@@ -19,6 +17,8 @@ import java.util.Date;
 @org.hibernate.annotations.Table(appliesTo = "sys_browse_log", comment = "浏览日志表")
 public class SysBrowseLog {
     @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(length = 64)
     private String id;
     @Column(columnDefinition = "varchar(47) COMMENT '粉丝混淆昵称'")
