cca587f8250aed5dd4f8ec1c3e61cd6cb27cdca6
ChenJW <1090262395@qq.com>
2020-10-06 13:57:33 +0800

    阿迪双十一

--- custom-item-project/src/main/java/com/duomai/project/api/gateway/QLApiExecuteHandler.java
+++ custom-item-project/src/main/java/com/duomai/project/api/gateway/QLApiExecuteHandler.java
@@ -48,7 +48,7 @@ public class QLApiExecuteHandler {
         map.put("wx.dz.browseBaby.list.load",DmBrowseBabyListExecute.class); //浏览宝贝列表 cjw
         map.put("wx.dz.clickTo.browse",DmClickToBrowseExecute.class); //浏览宝贝送抽奖次数 cjw
         map.put("wx.dz.tools.browse.baby",DmBrowseBabySaveDelExecute.class); //封网新增删除浏览宝贝接口 cjw
-        map.put("wx.dz.tools.big.wheel.",DmCusBigWheelSaveDelExecute.class); //封网新增删除尖货大咖活动接口 cjw
+        map.put("wx.dz.tools.big.wheel",DmCusBigWheelSaveDelExecute.class); //封网新增删除尖货大咖活动接口 cjw
 
 
     }
