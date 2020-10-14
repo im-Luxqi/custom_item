d3ccaadd1a0b66c16133c958450b09a26d20d92e
wangxingqi <im_luxqi@163.com>
2020-10-14 09:37:26 +0800

    微调

--- custom-item-project/src/main/java/com/duomai/project/product/TestController.java
+++ custom-item-project/src/main/java/com/duomai/project/product/TestController.java
@@ -8,6 +8,7 @@ import com.duomai.project.product.general.repository.SysAwardRepository;
 import com.duomai.project.product.general.repository.SysLuckyChanceRepository;
 import com.taobao.api.ApiException;
 import com.taobao.api.DefaultTaobaoClient;
+import com.taobao.api.response.AlibabaBenefitSendResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,6 +51,10 @@ public class TestController {
 
     @GetMapping(value = "wxq")
     public String test() throws ApiException {
+
+        taobaoAPIService.isMember("夏01p2dBwi4yiMon9yxJTfxmCkprZqrpmO3kFuSpghDIeqQ=");
+        AlibabaBenefitSendResponse alibabaBenefitSendResponse = taobaoAPIService.sendTaobaoCoupon("AAEA4uHTAMeks3ANjnKYXoKb", "03634718fd484633b4de2ec6bea5ee98");
+
         return "success";
     }
 
