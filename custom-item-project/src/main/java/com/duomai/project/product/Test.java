b9c27c251a90ef560030e89ac9c696402dec4f6e
ChenJW <1090262395@qq.com>
2020-10-14 08:55:37 +0800

    阿迪双十一

--- custom-item-project/src/main/java/com/duomai/project/product/Test.java
+++ custom-item-project/src/main/java/com/duomai/project/product/Test.java
@@ -11,12 +11,38 @@ public class Test {
 
         //生成sign
         String a = "{\"method\":\"wx.dz.tools.browse.baby\",\"sysCommodity\":[{\"name\":\"NITE JOGGER EF5402,男女经典运动鞋\",\"numId\":611446977762,\"type\":\"GOODS\",\"img\":\"https://img.alicdn.com/imgextra/i1/446338500/O1CN01okCPJC2Cf3AOCXFUv-446338500.jpg_430x430q90.jpg\",\"price\":1299,\"createTime\":\"2020-10-13 00:00:00\",\"commoditySort\":\"total\"},{\"name\":\"MICROPACER_R1 G27934,男鞋经典运动鞋\",\"numId\":591916480352,\"type\":\"GOODS\",\"img\":\"https://img.alicdn.com/imgextra/i4/6000000000093/O1CN01tByuOx1CYdPNrhOU6_!!6000000000093-0-tbvideo.jpg_600x600.jpg\",\"price\":1799,\"createTime\":\"2020-10-13 00:00:00\",\"commoditySort\":\"total\"},{\"name\":\"PW SOLAR HU NMD,男女经典运动鞋\",\"numId\":579198018067,\"type\":\"GOODS\",\"img\":\"https://img.alicdn.com/imgextra/i2/6000000005917/O1CN01SLElwj1ta26jnhBrc_!!6000000005917-0-tbvideo.jpg_600x600.jpg\",\"price\":1899,\"createTime\":\"2020-10-13 00:00:00\",\"commoditySort\":\"total\"}]}";
+        
+        String b = "{" +
+                "            \"method\":\"wx.dz.tools.big.wheel\"," +
+                "            \"bigWheel\":[" +
+                "                {" +
+                "                    \"title\":\"测试1\"," +
+                "                    \"context\":\"asd\"," +
+                "                    \"img\":\"图片1\"," +
+                "                    \"startTime\":\"2020-10-06\"," +
+                "                    \"endTime\":\"2020-10-11\"," +
+                "                    \"flyLink\":\"asd\"," +
+                "                    \"alias\":\"asd\"" +
+                "                }," +
+                "                {" +
+                "                    \"title\":\"测试2\"," +
+                "                    \"context\":\"asd\"," +
+                "                    \"img\":\"图片2\"," +
+                "                    \"startTime\":\"2020-10-06\"," +
+                "                    \"endTime\":\"2020-10-11\"," +
+                "                    \"flyLink\":\"asd\"," +
+                "                    \"alias\":\"asd\"" +
+                "                }" +
+                "            ]" +
+                "        }";
+       String c = b.replaceAll(" ","");
         String zhString ="";
         zhString = "20ee7d6400dda1c9622699123af2c2c8"+"admjson"+
-                EncryptUtil.adjustURLEncoderForJsEncodeURIComponent(a)
-                +"appkey"+"21699045"+"m"+"wx.dz.tools.browse.baby"+"timestamp"+"1602504154396"+"20ee7d6400dda1c9622699123af2c2c8";
+                EncryptUtil.adjustURLEncoderForJsEncodeURIComponent(c)
+                +"appkey"+"21699045"+"m"+"wx.dz.tools.big.wheel"+"timestamp"+"1602504154396"+"20ee7d6400dda1c9622699123af2c2c8";
         String newString = MD5Utils.getMD5(zhString.toLowerCase().getBytes("UTF-8"));
         System.out.println(newString);
+
 
     }
 
