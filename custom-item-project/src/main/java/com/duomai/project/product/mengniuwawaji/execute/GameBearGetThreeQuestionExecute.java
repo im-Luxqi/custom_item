0606cc3fa561bd49e30dfed879f538958bd41ec9
wangxingqi <im_luxqi@163.com>
2020-12-12 11:26:25 +0800

    调整

--- custom-item-project/src/main/java/com/duomai/project/product/mengniuwawaji/execute/GameBearGetThreeQuestionExecute.java
+++ custom-item-project/src/main/java/com/duomai/project/product/mengniuwawaji/execute/GameBearGetThreeQuestionExecute.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -31,10 +32,10 @@ public class GameBearGetThreeQuestionExecute implements IApiExecute {
     public YunReturnValue ApiExecute(ApiSysParameter sysParm, HttpServletRequest request, HttpServletResponse response) throws Exception {
         List<SysBearQuestion> allQuestion = projectHelper.getAllQuestion();
         List<SysBearQuestion> three = new ArrayList<>();
-        Random random = new Random();
-        three.add(allQuestion.get(random.nextInt(allQuestion.size())));
-        three.add(allQuestion.get(random.nextInt(allQuestion.size())));
-        three.add(allQuestion.get(random.nextInt(allQuestion.size())));
+        Collections.shuffle(allQuestion);
+        three.add(allQuestion.get(0));
+        three.add(allQuestion.get(1));
+        three.add(allQuestion.get(2));
         return YunReturnValue.ok(three, "白熊 获取三道新题目");
     }
 }
